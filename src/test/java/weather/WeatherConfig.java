0fc4398e8967e3c85e8c1f0ab61359b0e06acb70
denispeganovri <denis.peganov@river-island.com>
2024-07-08 17:20:22 +0200

    Add tests

---
+++ src/test/java/weather/WeatherConfig.java
@@ -0,0 +1,7 @@
+package weather;
+
+public class WeatherConfig {
+    public static final String API_KEY = "27ae9dd0ff074d28a2c182319240807";
+    public static final String BASE_URI = "http://api.weatherapi.com/v1/current.json";
+}
+
